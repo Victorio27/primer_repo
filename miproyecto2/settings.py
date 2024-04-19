bedb4f2fca00072870b72502d5067a9ea60166c9
Victorio Bussetti <bussettivictorio@gmail.com>
2024-04-19 18:24:06 -0300

    avatares en todas las vistas

--- miproyecto2/settings.py
+++ miproyecto2/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'AppCoder.context_processors.avatar_context',
             ],
         },
     },
